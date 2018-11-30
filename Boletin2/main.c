61d9570c7ff59eaed902543f634b4272fc10371f
Alejandro García Cortado <alejandrogarciacortado@gmail.com>
2018-11-30 12:02:11 +0100

    Empezar 2.0

---
+++ Boletin2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    int opcion=0;
+
+    do {
+        printf("####Backtracking####\n");
+        printf("1.Ejercicio 1\n");
+        printf("2.Ejercicio 2\n");
+        printf("3.Salir");
+        scanf("%d",&opcion);
+        fflush(stdin):
+
+        switch(opcion)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+
+
+        }
+
+    }while(opcion==3);
+    return 0;
+}
